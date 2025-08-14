92fb67e690d657a774c1afbafb69addf6e8ba1d9
Ciaran Dunne <ciaran.dunne2@gmail.com>
2025-08-13 22:05:37 -0400

    Regina

--- app.py
+++ app.py
@@ -2,20 +2,10 @@ import streamlit as st
 
 st.title("Hello World App")
 
-st.write("Hello, Streamlit!")
+st.write("Hello, Regina!")
 
 name = st.text_input("What's your name?")
 
 if name:
     st.write(f"Howiya, {name}!")
 
-import streamlit as st
-
-st.title("Hello World App")
-
-st.write("Hello, world!")
-
-name = st.text_input("What's your name?")
-
-if name:
-    st.write(f"Hello, {name}!")
